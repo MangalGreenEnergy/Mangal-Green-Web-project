ba3f497cb8085c88b035fd5d661935ecc6a97f01
sanket <gaikwad30sanket@gmail.com>
2024-04-03 10:54:57 +0530

    some other changes

--- client/src/features/product-list/ProductList.js
+++ client/src/features/product-list/ProductList.js
@@ -214,7 +214,7 @@ export default function ProductList() {
         </Transition.Root>
 
         <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
+          <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-2">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900">All Products</h1>
 
             <div className="flex items-center">
