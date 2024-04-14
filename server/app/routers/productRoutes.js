127ebc3af8b59499a706a038ca9b5a1cfb762c96
Surykant Kasare <kasare9096464112@gmail.com>
2024-04-14 17:32:03 +0530

    update

--- server/app/routers/productRoutes.js
+++ server/app/routers/productRoutes.js
@@ -7,9 +7,9 @@ router.get('/', productController.getAllProducts);
 router.post('/add', productController.createProduct);
 router.get('/categories', productController.getAllCategories); // New route for fetching all categories
 router.get('/category/:category', productController.getProductsByCategory);
-router.get('/:id', productController.getProductById);
 router.get("/check", (req, res) => {
     res.send("Hello, I am live");
-  });
-  
+});
+
+router.get('/:id', productController.getProductById);
 module.exports = router;
