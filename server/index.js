747ccd3cfb4554555464281688c8ce82b7d394dd
Surykant Kasare <kasare9096464112@gmail.com>
2024-04-14 13:24:18 +0530

    update on products

--- server/index.js
+++ server/index.js
@@ -1,19 +1,20 @@
-//jshint esversion:6
-
 const express = require("express");
-const connectDB = require("./config/db"); // Import db.js
+const cors = require("cors");
+const connectDB = require("./config/db");
 const bodyParser = require("body-parser");
 const userRoutes = require("./app/routers/userRoutes");
-const productRoutes = require('./app/routers/productRoutes')
+const productRoutes = require("./app/routers/productRoutes");
+
 const app = express();
+
+app.use(cors()); // Enable CORS
+
 app.use(bodyParser.json());
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 
-// Connect to MongoDB Atlas
 connectDB();
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
